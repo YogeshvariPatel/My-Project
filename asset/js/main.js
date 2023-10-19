b488cc9705a99b16152c3dee8476a28bc0637e96
vikas6938 <vikasborse000@email.com>
2023-10-19 11:10:47 +0530

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,10 @@
+// slick slider testimonial
+
+$('.autoplay').slick({
+  slidesToShow:1,
+  slideToScroll:1,
+  autoplay:true,
+  autoplaySpeed:1000,
+ 
+
+});
---
+++ asset/project/coffee king/asset/js/main.js
@@ -0,0 +1,452 @@
+
+
+//masonay start//
+
+var gallery = document.querySelector("#gallery");
+var getVal = function (elem, style) {
+  return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
+};
+var getHeight = function (item) {
+  return item.querySelector(".content").getBoundingClientRect().height;
+};
+var resizeAll = function () {
+  var altura = getVal(gallery, "grid-auto-rows");
+  var gap = getVal(gallery, "grid-row-gap");
+  gallery.querySelectorAll(".gallery-item").forEach(function (item) {
+    var el = item;
+    el.style.gridRowEnd =
+      "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+  });
+};
+gallery.querySelectorAll("img").forEach(function (item) {
+  item.classList.add("byebye");
+  if (item.complete) {
+    console.log(item.src);
+  } else {
+    item.addEventListener("load", function () {
+      var altura = getVal(gallery, "grid-auto-rows");
+      var gap = getVal(gallery, "grid-row-gap");
+      var gitem = item.parentElement.parentElement;
+      gitem.style.gridRowEnd =
+        "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+      item.classList.remove("byebye");
+    });
+  }
+});
+window.addEventListener("resize", resizeAll);
+gallery.querySelectorAll(".gallery-item").forEach(function (item) {
+  item.addEventListener("click", function () {
+    item.classList.toggle("full");
+  });
+});
+
+
+
+//--------masonary end------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------faq js start-----//
+
+
+let questions = document.querySelectorAll(".question")
+let answers = document.querySelectorAll(".answer")
+
+questions.forEach((question)=>{
+    question.addEventListener("click",()=>{
+        const answer = question.closest(".faq").querySelector(".answer")
+        answer.classList.toggle("display")
+        answers.forEach((a)=>{
+            a!= answer && a.classList.remove("display")
+        })
+    })
+})
+//------faq js end-----//
+
+
+
+
+//---fancy box js start--//
+// Fancybox.bind("[data-fancybox]", {
+// });
+
+//---fancy box js end---//
+
+
+
+
+
+
+
+
+
+
+
+
+//----headline js start---//
+
+
+
+jQuery(document).ready(function($){
+	//set animation timing
+	var animationDelay = 2500,
+		//loading bar effect
+		barAnimationDelay = 3800,
+		barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
+		//letters effect
+		lettersDelay = 50,
+		//type effect
+		typeLettersDelay = 150,
+		selectionDuration = 500,
+		typeAnimationDelay = selectionDuration + 800,
+		//clip effect 
+		revealDuration = 600,
+		revealAnimationDelay = 1500;
+	
+	initHeadline();
+	
+
+	function initHeadline() {
+		//insert <i> element for each letter of a changing word
+		singleLetters($('.cd-headline.letters').find('b'));
+		//initialise headline animation
+		animateHeadline($('.cd-headline'));
+	}
+
+	function singleLetters($words) {
+		$words.each(function(){
+			var word = $(this),
+				letters = word.text().split(''),
+				selected = word.hasClass('is-visible');
+			for (i in letters) {
+				if(word.parents('.rotate-2').length > 0) letters[i] = '<em>' + letters[i] + '</em>';
+				letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>': '<i>' + letters[i] + '</i>';
+			}
+		    var newLetters = letters.join('');
+		    word.html(newLetters).css('opacity', 1);
+		});
+	}
+
+	function animateHeadline($headlines) {
+		var duration = animationDelay;
+		$headlines.each(function(){
+			var headline = $(this);
+			
+			if(headline.hasClass('loading-bar')) {
+				duration = barAnimationDelay;
+				setTimeout(function(){ headline.find('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+			} else if (headline.hasClass('clip')){
+				var spanWrapper = headline.find('.cd-words-wrapper'),
+					newWidth = spanWrapper.width() + 10
+				spanWrapper.css('width', newWidth);
+			} else if (!headline.hasClass('type') ) {
+				//assign to .cd-words-wrapper the width of its longest word
+				var words = headline.find('.cd-words-wrapper b'),
+					width = 0;
+				words.each(function(){
+					var wordWidth = $(this).width();
+				    if (wordWidth > width) width = wordWidth;
+				});
+				headline.find('.cd-words-wrapper').css('width', width);
+			};
+
+			//trigger animation
+			setTimeout(function(){ hideWord( headline.find('.is-visible').eq(0) ) }, duration);
+		});
+	}
+
+	function hideWord($word) {
+		var nextWord = takeNext($word);
+		
+		if($word.parents('.cd-headline').hasClass('type')) {
+			var parentSpan = $word.parent('.cd-words-wrapper');
+			parentSpan.addClass('selected').removeClass('waiting');	
+			setTimeout(function(){ 
+				parentSpan.removeClass('selected'); 
+				$word.removeClass('is-visible').addClass('is-hidden').children('i').removeClass('in').addClass('out');
+			}, selectionDuration);
+			setTimeout(function(){ showWord(nextWord, typeLettersDelay) }, typeAnimationDelay);
+		
+		} else if($word.parents('.cd-headline').hasClass('letters')) {
+			var bool = ($word.children('i').length >= nextWord.children('i').length) ? true : false;
+			hideLetter($word.find('i').eq(0), $word, bool, lettersDelay);
+			showLetter(nextWord.find('i').eq(0), nextWord, bool, lettersDelay);
+
+		}  else if($word.parents('.cd-headline').hasClass('clip')) {
+			$word.parents('.cd-words-wrapper').animate({ width : '2px' }, revealDuration, function(){
+				switchWord($word, nextWord);
+				showWord(nextWord);
+			});
+
+		} else if ($word.parents('.cd-headline').hasClass('loading-bar')){
+			$word.parents('.cd-words-wrapper').removeClass('is-loading');
+			switchWord($word, nextWord);
+			setTimeout(function(){ hideWord(nextWord) }, barAnimationDelay);
+			setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('is-loading') }, barWaiting);
+
+		} else {
+			switchWord($word, nextWord);
+			setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+		}
+	}
+
+	function showWord($word, $duration) {
+		if($word.parents('.cd-headline').hasClass('type')) {
+			showLetter($word.find('i').eq(0), $word, false, $duration);
+			$word.addClass('is-visible').removeClass('is-hidden');
+
+		}  else if($word.parents('.cd-headline').hasClass('clip')) {
+			$word.parents('.cd-words-wrapper').animate({ 'width' : $word.width() + 10 }, revealDuration, function(){ 
+				setTimeout(function(){ hideWord($word) }, revealAnimationDelay); 
+			});
+		}
+	}
+
+	function hideLetter($letter, $word, $bool, $duration) {
+		$letter.removeClass('in').addClass('out');
+		
+		if(!$letter.is(':last-child')) {
+		 	setTimeout(function(){ hideLetter($letter.next(), $word, $bool, $duration); }, $duration);  
+		} else if($bool) { 
+		 	setTimeout(function(){ hideWord(takeNext($word)) }, animationDelay);
+		}
+
+		if($letter.is(':last-child') && $('html').hasClass('no-csstransitions')) {
+			var nextWord = takeNext($word);
+			switchWord($word, nextWord);
+		} 
+	}
+
+	function showLetter($letter, $word, $bool, $duration) {
+		$letter.addClass('in').removeClass('out');
+		
+		if(!$letter.is(':last-child')) { 
+			setTimeout(function(){ showLetter($letter.next(), $word, $bool, $duration); }, $duration); 
+		} else { 
+			if($word.parents('.cd-headline').hasClass('type')) { setTimeout(function(){ $word.parents('.cd-words-wrapper').addClass('waiting'); }, 200);}
+			if(!$bool) { setTimeout(function(){ hideWord($word) }, animationDelay) }
+		}
+	}
+
+	function takeNext($word) {
+		return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+	}
+
+	function takePrev($word) {
+		return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
+	}
+
+	function switchWord($oldWord, $newWord) {
+		$oldWord.removeClass('is-visible').addClass('is-hidden');
+		$newWord.removeClass('is-hidden').addClass('is-visible');
+	}
+});
+
+//---headline js end-----//
+
+
+
+
+
+
+
+
+
+//---counter js start-----//
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerText;
+
+    const increment = target / 200;
+    console.log(increment);
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
+
+//---counter js end-----------//
+
+
+
+//pointer js start//
+
+
+const cursor = document.querySelector("#cursor");
+const cursorBorder = document.querySelector("#cursor-border");
+const cursorPos = { x: 0, y: 0 };
+const cursorBorderPos = { x: 0, y: 0 };
+
+document.addEventListener("mousemove", (e) => {
+  cursorPos.x = e.clientX;
+  cursorPos.y = e.clientY;
+
+  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
+
+requestAnimationFrame(function loop() {
+  const easting = 8;
+  cursorBorderPos.x += (cursorPos.x - cursorBorderPos.x) / easting;
+  cursorBorderPos.y += (cursorPos.y - cursorBorderPos.y) / easting;
+
+  cursorBorder.style.transform = `translate(${cursorBorderPos.x}px, ${cursorBorderPos.y}px)`;
+  requestAnimationFrame(loop);
+});
+
+document.querySelectorAll("[data-cursor]").forEach((item) => {
+  item.addEventListener("mouseover", (e) => {
+    if (item.dataset.cursor === "pointer") {
+      cursorBorder.style.backgroundColor = "rgba(255, 255, 255, .6)";
+      cursorBorder.style.setProperty("--size", "30px");
+    }
+    if (item.dataset.cursor === "pointer2") {
+      cursorBorder.style.backgroundColor = "white";
+      cursorBorder.style.mixBlendMode = "difference";
+      cursorBorder.style.setProperty("--size", "80px");
+    }
+  });
+  item.addEventListener("mouseout", (e) => {
+    cursorBorder.style.backgroundColor = "unset";
+    cursorBorder.style.mixBlendMode = "unset";
+    cursorBorder.style.setProperty("--size", "50px");
+  });
+});
+
+
+//pointer js end//
+
+
+
+
+
+
+
+
+$(document).ready(function (){
+    $(' .fa-bars').click(function (){
+        $('.menu').toggleClass('active');
+    });
+    $(window).on("load",function(){
+        $('.preloader').fadeOut(2000);
+    });
+    $(window).scroll(function (){
+        if (scrollY > 200) {
+            $('header').addClass('sticky');
+            $('#top').fadeIn(2000);
+        }
+        else {
+            $('header').removeClass('sticky');
+            $('#top').fadeOut(2000);
+
+        }
+    });
+    $('#top').click(function(){
+        $('html').animate({scrollTop:0});
+    });
+    $('#setting').click(function(){
+        $('.color-changer').slideToggle(1000);
+
+    });
+    $('#brown').click(switchbrown);
+    $('#pink').click(switchpink);
+    $('#grey').click(switchgrey);
+
+    function switchbrown(){
+        $(':root').css('--primary','#c9ada7');
+    }
+    function switchpink(){
+        $('body').attr('class','pink');
+    }
+    function switchgrey(){
+        $('body').attr('class','grey');
+    }
+
+    $('.owl-carousel').owlCarousel({
+        dots: false,
+        loop: true,
+        margin: 20,
+        stagepadding: 30,
+        rtl: true,
+        nav: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+
+    $('.testi').slick({
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+
+    });
+
+
+});
